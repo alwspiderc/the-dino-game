5192424dcbf243755234953b2d0db8c6f07c81f4
alwspiderc <alwesterc@gmail.com>
2022-03-17 13:09:36 -0300

    correçao de bugs

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function createCactus() {
       // Game over
       clearInterval(leftInterval);
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1> <button class="botao" type="button">Jogar novamente</button>';
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
